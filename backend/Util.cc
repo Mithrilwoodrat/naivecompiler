200c6cca090ed1def67d3222e3c3603d649f6b74
mithrilwoodrat <mithrilwoodrat@gmail.com>
2017-03-02 14:27:29 +0800

    add micro

--- backend/Util.cc
+++ backend/Util.cc
@@ -5,8 +5,9 @@ namespace naivescript
 
 namespace util
 {
+#ifdef __cplusplus
 extern "C" {
-
+#endif
 uint32_t getStructType(uint8_t* data) {
     return *((uint32_t*)(data));
 }
@@ -14,9 +15,9 @@ uint32_t getStructType(uint8_t* data) {
 uint32_t getVarStructSize(uint8_t* data) {
     return *((uint32_t*)(data) + 2);
 }
-
+#ifdef __cplusplus
 }
+#endif
 }
-
 }
 
--- backend/Util.h
+++ backend/Util.h
@@ -10,13 +10,15 @@ namespace naivescript
 
 namespace util
 {
+#ifdef __cplusplus
 extern "C"
+#endif
 {
 uint32_t getStructType(uint8_t *s);
 uint32_t getVarStructSize(uint8_t* data);
-
+#ifdef __cplusplus
 }
+#endif
 }
-
 }
 #endif
