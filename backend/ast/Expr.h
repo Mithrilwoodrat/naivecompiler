e664a81ea24dd5545af6fb269964619bdbbcae80
mithrilwoodrat <mithrilwoodrat@gmail.com>
2018-06-18 23:20:04 +0800

    add Expr Base Class

---
+++ backend/ast/Expr.h
@@ -0,0 +1,21 @@
+#ifndef EXPR_H
+#define EXPR_H
+
+#include "Statement.h"
+#include "SerializeStructure.h"
+#include "NodeFactory.h"
+#include "Util.h"
+
+namespace naivescript{
+
+class Visitor;
+
+class Expr: public Statement
+{
+public:
+	Expr(serialize::NodeType type) : Statement(type) {}
+};
+
+};
+
+#endif
