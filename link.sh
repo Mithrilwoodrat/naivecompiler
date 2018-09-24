a0678444805b215f76cf99c1ad7feaf0bed29d75
mithrilwoodrat <mithrilwoodrat@gmail.com>
2018-09-24 15:43:15 +0800

    link script support os x

--- link.sh
+++ link.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ld -o $2 -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o -lc $1 /usr/lib/x86_64-linux-gnu/crtn.o
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+     ld -o $2 -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o -lc $1 /usr/lib/x86_64-linux-gnu/crtn.o
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ld -macosx_version_min 10.1 -o $2  /usr/lib/libSystem.dylib /usr/lib/crt1.o $1
+else
+    echo "unknow platform"
+fi
+
