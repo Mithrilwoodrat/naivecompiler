8c269f98c05ee4ed219aa4084265d5cb1be2f6cc
mithrilwoodrat <mithrilwoodrat@gmail.com>
2019-06-10 20:49:04 +0800

    add testcase

---
+++ tests/testnestedif.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int i;
+    i = 0;
+    while (1) {
+        i = i + 1;
+        if (i > 1) {
+            if (i > 10) {
+                i = i+1;
+                break;
+            }
+        }
+	}
+    return 0;    
+}
